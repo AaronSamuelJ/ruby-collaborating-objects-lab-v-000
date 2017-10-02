bc2b4a4c6c933848262bbc81be5ca7e4b0b5e94a
Aaron Samuel <aasamuelj@gmail.com>
2017-10-02 05:47:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,11 @@ class Song
   end
   def self.new_by_filename(filename)
     spl = filename.split(" - ")
-    binding.pry
+    artist_name = spl[0]
+    song_name = spl[1]
+    artist = Artist.find_or_create_by_name(artist_name)
+    song = Song.new(song_name)
+    artist.add_song(song)
+    song
   end
 end 
