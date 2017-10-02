185cdb9c1740dcdd84970afb107c6540c23583d6
Aaron Samuel <aasamuelj@gmail.com>
2017-10-02 05:43:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,9 @@ class Song
   attr_accessor :name, :artist 
   def initialize(name)
     @name = name 
-    
   end
-
-
+  def self.new_by_filename(filename)
+    spl = filename.split(" - ")
+    binding.pry
+  end
 end 
